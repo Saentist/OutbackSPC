332ceef4ecbe6b89e419c4bb5f2538d8f4d38dfa
dvokoun <david.vokoun@bizconsult.at>
2023-04-26 08:13:46 +0200

    Working

--- dbushelper.py
+++ dbushelper.py
@@ -107,13 +107,13 @@ class DbusHelper:
         if self.devType == 'solarcharger':
             # Update SOC, DC and System items
             print('solarcharger 1')
-            self._dbusService["/Hub/ChargeVoltage"] = round(self.inverter.a11pvInputVoltage, 2)
-            self._dbusService["/Dc/0/Voltage"] = round(self.inverter.a11pvInputVoltage, 2)
-            self._dbusService["/Dc/0/Current"] = round(self.inverter.a11pvInputCurrent, 2)
+            #self._dbusService["/Hub/ChargeVoltage"] = round(self.inverter.a11pvInputVoltage, 2)
+            #self._dbusService["/Dc/0/Voltage"] = round(self.inverter.a11pvInputVoltage, 2)
+            #self._dbusService["/Dc/0/Current"] = round(self.inverter.a11pvInputCurrent, 2)
             #self._dbusService["/Dc/0/Power"] = round(self.inverter.a11pvInputPower, 2)
-            self._dbusService["/Pv/I"] = round(self.inverter.a11pvInputCurrent, 2)
-            self._dbusService["/Pv/V"] = round(self.inverter.a11pvInputVoltage, 2)
-            self._dbusService["/Load/I"] = round(self.inverter.a11pvInputCurrent, 2)
+            #self._dbusService["/Pv/I"] = round(self.inverter.a11pvInputCurrent, 2)
+            #self._dbusService["/Pv/V"] = round(self.inverter.a11pvInputVoltage, 2)
+            #self._dbusService["/Load/I"] = round(self.inverter.a11pvInputCurrent, 2)
             print('solarcharger 2')
 
         if self.devType == 'vebus':
