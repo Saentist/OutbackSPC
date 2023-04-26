2d1bed58da4538ee76c644b4aa43a38b504702a9
dvokoun <david.vokoun@bizconsult.at>
2023-04-26 13:48:31 +0200

    Working II

--- dbushelper.py
+++ dbushelper.py
@@ -124,10 +124,10 @@ class DbusHelper:
             #self._dbusService["/Hub/ChargeVoltage"] = round(self.inverter.a11pvInputVoltage, 2)
             self._dbusService["/Dc/0/Voltage"] = round(self.inverter.a11pvInputVoltage, 2)
             self._dbusService["/Dc/0/Current"] = round(self.inverter.a11pvInputCurrent, 2)
-            #self._dbusService["/Dc/0/Power"] = round(self.inverter.a11pvInputPower, 2)
+            self._dbusService["/Dc/0/Power"] = round(self.inverter.a11pvInputPower, 2)
             self._dbusService["/Pv/I"] = round(self.inverter.a11pvInputCurrent, 2)
             self._dbusService["/Pv/V"] = round(self.inverter.a11pvInputVoltage, 2)
-            self._dbusService["/Load/I"] = round(1234, 2)
+            # self._dbusService["/Load/I"] = round(1234, 2)
             # print('solarcharger 2')
 
         if self.devType == 'vebus':
