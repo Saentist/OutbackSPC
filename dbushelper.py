cd175c798208baa9ada3701e04e8bcf0c60f733a
dvokoun <david.vokoun@bizconsult.at>
2023-04-29 20:10:35 +0200

    Working II

--- dbushelper.py
+++ dbushelper.py
@@ -207,7 +207,7 @@ class DbusHelper:
             # self._dbusService['/NrOfTrackers'] = 1                                                  # <- number of trackers
             self._dbusService['/Pv/0/V'] = round(self.inverter.a11pvInputVoltage, 2)                # <- PV array voltage from 1st tracker
             self._dbusService['/Pv/0/P'] = round(self.inverter.a11pvInputPower, 2)                  # <- PV array power (Watts) from 1st tracker
-            # self._dbusService['/Yield/Power'] = 1                                                 # <- PV array power (Watts)
+            self._dbusService['/Yield/Power'] = 123                                                 # <- PV array power (Watts)
             # self._dbusService['/Yield/User'] = 1                                                  # <- Total kWh produced (user resettable)
             # self._dbusService['/Yield/System'] = 1                                                # <- Total kWh produced (not resettable)
             # self._dbusService['/MppOperationMode'] = 2                                              # <- 0 = Off 1 = Voltage or Current limited 2 = MPPT Tracker active
