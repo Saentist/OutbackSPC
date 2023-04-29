23ba16c9d6cefb6201f5a6242d4e85c7d8a3708c
dvokoun <david.vokoun@bizconsult.at>
2023-04-29 20:00:53 +0200

    Working II

--- dbushelper.py
+++ dbushelper.py
@@ -91,6 +91,8 @@ class DbusHelper:
         elif self.devType == 'multi':
             self.inverter.type = 'multi'
             self._dbusService.add_path("/Yield/Power", None, writeable=True, gettextcallback=_w, )
+            self._dbusService.add_path("/Ac/In/1/L1/P", None, writeable=True, gettextcallback=_w, )
+            self._dbusService.add_path("/Ac/In/1/L1/I", None, writeable=True, gettextcallback=_a, )
             self._dbusService.add_path("/Ac/In/1/L1/V", None, writeable=True, gettextcallback=_v, )
             self._dbusService.add_path("/Ac/In/1/L1/F", None, writeable=True, gettextcallback=_h, )
             self._dbusService.add_path("/Ac/Out/L1/P", None, writeable=True, gettextcallback=_w, )
