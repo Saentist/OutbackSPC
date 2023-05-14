8236bef83102c03005d479244868cc97681aa729
dvokoun <david.vokoun@bizconsult.at>
2023-05-14 22:06:42 +0200

    removed old config reading

--- utils.py
+++ utils.py
@@ -39,7 +39,7 @@ degree_sign = "\N{DEGREE SIGN}"
 #LINEAR_LIMITATION_ENABLE = "True" == config["DEFAULT"]["LINEAR_LIMITATION_ENABLE"]
 #MAX_BATTERY_CHARGE_CURRENT = float(config["DEFAULT"]["MAX_BATTERY_CHARGE_CURRENT"])
 #CELL_VOLTAGES_WHILE_CHARGING = _get_list_from_config("DEFAULT", "CELL_VOLTAGES_WHILE_CHARGING", lambda v: float(v))
-PUBLISH_CONFIG_VALUES = int(config["DEFAULT"]["PUBLISH_CONFIG_VALUES"])
+#PUBLISH_CONFIG_VALUES = int(config["DEFAULT"]["PUBLISH_CONFIG_VALUES"])
 
 def constrain(val, min_val, max_val):
     if min_val > max_val:
