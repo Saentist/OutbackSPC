cfc43ee25060748fa8ab7cc6791af5ed6cda8b1a
dvokoun <david.vokoun@bizconsult.at>
2023-05-16 20:58:56 +0200

    development

--- outbackbt.py
+++ outbackbt.py
@@ -62,7 +62,7 @@ class OutbackBtDev(DefaultDelegate, Thread):
             try:
                 outbackService00001810 = self.bt.getServiceByUUID('00001810-0000-1000-8000-00805f9b34fb')
                 outbackService00001811 = self.bt.getServiceByUUID('00001811-0000-1000-8000-00805f9b34fb')
-                outbackService0000180a = self.bt.getServiceByUUID('0000180a-0000-1000-8000-00805f9b34fb')
+                #outbackService0000180a = self.bt.getServiceByUUID('0000180a-0000-1000-8000-00805f9b34fb')
 
                 # kann  nicht gelesen werden, daher auskommentiert
                 #outbackService00001801 = self.bt.getServiceByUUID('00001801-0000-1000-8000-00805f9b34fb')
@@ -99,7 +99,7 @@ class OutbackBtDev(DefaultDelegate, Thread):
                 if self.debug:
                     print(outbackService00001810a01Data)
                     print(outbackService00001810a02Data)
-                    print(outbackService00001810a03Data)
+                    print("a03: " + outbackService00001810a03Data)
                     print(outbackService00001810a04Data)
                     print(outbackService00001810a05Data)
                     print(outbackService00001810a06Data)
@@ -120,7 +120,7 @@ class OutbackBtDev(DefaultDelegate, Thread):
                 outbackService00001811a13Data = self.getExtractData(outbackService00001811a13.read())
                 outbackService00001811a14Data = self.getExtractData(outbackService00001811a14.read())
                 if self.debug:
-                    print(outbackService00001811a11Data)
+                    print("a11: " +  outbackService00001811a11Data)
                     print(outbackService00001811a12Data)
                     print(outbackService00001811a13Data)
                     print(outbackService00001811a14Data)
