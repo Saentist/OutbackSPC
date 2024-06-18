832aa91b9c44acff350ba93ccd2738f67ead3fa2
dvokoun <david.vokoun@bizconsult.at>
2024-06-18 22:37:33 +0200

    generalDataCB nur einmal mit allen Daten aufrufen

--- outbackbt.py
+++ outbackbt.py
@@ -121,8 +121,14 @@ class OutbackBtDev(DefaultDelegate, Thread):
                     #logger.info(outbackService0000180aa2aData)
 
                 # read our data
-                self.generalDataCallback(outbackService00001810a03Data, "a03")
-                self.generalDataCallback(outbackService00001811a11Data, "a11")
+                # self.generalDataCallback(outbackService00001810a03Data, "a03")
+                # self.generalDataCallback(outbackService00001811a11Data, "a11")
+                outbackServiceData = {
+                    "a03": outbackService00001810a03Data,
+                    "a11": outbackService00001811a11Data
+                }
+                self.generalDataCallback(outbackServiceData)
+
                 sleep(self.interval)
 
             except BTLEDisconnectError:
@@ -193,124 +199,131 @@ class OutbackBt(Inverter):
             return False
 
     def read_gen_data(self):
-        self.mutex.acquire()
+        with self.mutex:
 
-        if self.a03Data is None and self.a11Data is None and self.a29Data is None:
-            self.mutex.release()
-            logger.info("INFO >>> Currently no data at all")
-            return False
+            if self.a03Data is None and self.a11Data is None and self.a29Data is None:
+                self.mutex.release()
+                logger.info("INFO >>> Currently no data at all")
+                return False
 
-        if self.a03Data:
-            self.a03gridVoltage = self.a03Data[0]
-            self.a03gridFrequency = self.a03Data[1]
-            self.a03acOutputVoltage = self.a03Data[2] * 0.1
-            self.a03acFrequency = self.a03Data[3] * 0.1
-            self.a03acApparentPower = self.a03Data[4]
-            self.a03acActivePower = self.a03Data[5]
-            self.a03loadPercent = self.a03Data[6]
-            self.a03busVoltage = self.a03Data[7]
-            self.a03batteryVoltage = self.a03Data[8] * 0.01
-            self.a03batteryChargeCurrent = self.a03Data[9]
-            self.a03acOutputCurrent = self.a03acActivePower / self.a03acOutputVoltage
+            if self.a03Data:
+                self.a03gridVoltage = self.a03Data[0]
+                self.a03gridFrequency = self.a03Data[1]
+                self.a03acOutputVoltage = self.a03Data[2] * 0.1
+                self.a03acFrequency = self.a03Data[3] * 0.1
+                self.a03acApparentPower = self.a03Data[4]
+                self.a03acActivePower = self.a03Data[5]
+                self.a03loadPercent = self.a03Data[6]
+                self.a03busVoltage = self.a03Data[7]
+                self.a03batteryVoltage = self.a03Data[8] * 0.01
+                self.a03batteryChargeCurrent = self.a03Data[9]
+                self.a03acOutputCurrent = self.a03acActivePower / self.a03acOutputVoltage
 
-            # AUSGABE
-            if self.debug:
-                logger.info('collected values')
-                logger.info('a03gridVoltage => ' + str(self.a03gridVoltage))
-                logger.info('a03gridFrequency => ' + str(self.a03gridFrequency))
-                logger.info('a03acOutputVoltage => ' + str(self.a03acOutputVoltage))
-                logger.info('a03acFrequency => ' + str(self.a03acFrequency))
-                logger.info('a03acApparentPower => ' + str(self.a03acApparentPower))
-                logger.info('a03acActivePower => ' + str(self.a03acActivePower))
-                logger.info('a03loadPercent => ' + str(self.a03loadPercent))
-                logger.info('a03busVoltage => ' + str(self.a03busVoltage))
-                logger.info('a03batteryVoltage => ' + str(self.a03batteryVoltage))
-                logger.info('a03batteryChargeCurrent => ' + str(self.a03batteryChargeCurrent))
-                logger.info('calculated values')
-                logger.info('a03acOutputCurrent => ' + str(self.a03acOutputCurrent))
-
-        if self.a11Data:
-            self.a11unknown0 = self.a11Data[0]  #
-            self.a11unknown1 = self.a11Data[1]  #
-            self.a11unknown2 = self.a11Data[2]  #
-            self.a11unknown3 = self.a11Data[3]  #
-            self.a11unknown4 = self.a11Data[4]  #
-            self.a11unknown5 = self.a11Data[5]  #
-            self.a11pvInputVoltage = self.a11Data[6] * 0.1  # Volt
-            self.a11pvInputPower = self.a11Data[7]  # Watt
-            self.a11unknown8 = self.a11Data[8]  #
-            self.a11unknown9 = self.a11Data[9]  #
-
-            if self.a11pvInputPower > 0:
-                self.a11pvInputCurrent = self.a11pvInputPower / self.a11pvInputVoltage
-            else:
-                self.a11pvInputCurrent = 0
-
-            if self.debug:
                 # AUSGABE
-                logger.info('collected values')
-                logger.info('a11 unknown0 => ' + str(self.a11unknown0))
-                logger.info('a11 unknown1 => ' + str(self.a11unknown1))
-                logger.info('a11 unknown2 => ' + str(self.a11unknown2))
-                logger.info('a11 unknown3 => ' + str(self.a11unknown3))
-                logger.info('a11 unknown4 => ' + str(self.a11unknown4))
-                logger.info('a11 unknown5 => ' + str(self.a11unknown5))
-                logger.info('a11pvInputPower => ' + str(self.a11pvInputPower))
-                logger.info('a11pvInputVoltage => ' + str(self.a11pvInputVoltage))
-                logger.info('a11 unknown8 => ' + str(self.a11unknown8))
-                logger.info('a11 unknown9 => ' + str(self.a11unknown9))
-                logger.info('calculated values')
-                logger.info('a11pvInputCurrent => ' + str(self.a11pvInputCurrent))
+                if self.debug:
+                    logger.info('Collected values oa A03')
+                    logger.info('------------------------')
+                    logger.info('a03gridVoltage => ' + str(self.a03gridVoltage))
+                    logger.info('a03gridFrequency => ' + str(self.a03gridFrequency))
+                    logger.info('a03acOutputVoltage => ' + str(self.a03acOutputVoltage))
+                    logger.info('a03acFrequency => ' + str(self.a03acFrequency))
+                    logger.info('a03acApparentPower => ' + str(self.a03acApparentPower))
+                    logger.info('a03acActivePower => ' + str(self.a03acActivePower))
+                    logger.info('a03loadPercent => ' + str(self.a03loadPercent))
+                    logger.info('a03busVoltage => ' + str(self.a03busVoltage))
+                    logger.info('a03batteryVoltage => ' + str(self.a03batteryVoltage))
+                    logger.info('a03batteryChargeCurrent => ' + str(self.a03batteryChargeCurrent))
+                    logger.info('calculated values')
+                    logger.info('a03acOutputCurrent => ' + str(self.a03acOutputCurrent))
+                    logger.info('------------------------')
 
-        if self.a29Data:
-            # A29 Bereich
-            self.a29unknown0 = self.a29Data[0]  #
-            self.a29unknown1 = self.a29Data[1]  #
-            self.a29unknown2 = self.a29Data[2]  #
-            self.a29unknown3 = self.a29Data[3]  #
-            self.a29unknown4 = self.a29Data[4]  #
-            self.a29unknown5 = self.a29Data[5]  #
-            self.a29unknown6 = self.a29Data[6]  #
-            self.a29unknown7 = self.a29Data[7]  #
-            self.a29unknown8 = self.a29Data[8]  #
-            self.a29unknown9 = self.a29Data[9]  #
+            if self.a11Data:
+                self.a11unknown0 = self.a11Data[0]  #
+                self.a11unknown1 = self.a11Data[1]  #
+                self.a11unknown2 = self.a11Data[2]  #
+                self.a11unknown3 = self.a11Data[3]  #
+                self.a11unknown4 = self.a11Data[4]  #
+                self.a11unknown5 = self.a11Data[5]  #
+                self.a11pvInputVoltage = self.a11Data[6] * 0.1  # Volt
+                self.a11pvInputPower = self.a11Data[7]  # Watt
+                self.a11unknown8 = self.a11Data[8]  #
+                self.a11unknown9 = self.a11Data[9]  #
 
-            if self.debug:
-                # AUSGABE
-                logger.info('a29 unknown0 => ' + str(self.a29unknown0))
-                logger.info('a29 unknown1 => ' + str(self.a29unknown1))
-                logger.info('a29 unknown2 => ' + str(self.a29unknown2))
-                logger.info('a29 unknown3 => ' + str(self.a29unknown3))
-                logger.info('a29 unknown4 => ' + str(self.a29unknown4))
-                logger.info('a29 unknown5 => ' + str(self.a29unknown5))
-                logger.info('a29 unknown6 => ' + str(self.a29unknown6))
-                logger.info('a29 unknown7 => ' + str(self.a29unknown7))
-                logger.info('a29 unknown8 => ' + str(self.a29unknown8))
-                logger.info('a29 unknown9 => ' + str(self.a29unknown9))
+                if self.a11pvInputPower > 0:
+                    self.a11pvInputCurrent = self.a11pvInputPower / self.a11pvInputVoltage
+                else:
+                    self.a11pvInputCurrent = 0
 
-        self.mutex.release()
+                if self.debug:
+                    # AUSGABE
+                    logger.info('Collected values oa A11')
+                    logger.info('------------------------')
+                    logger.info('a11 unknown0 => ' + str(self.a11unknown0))
+                    logger.info('a11 unknown1 => ' + str(self.a11unknown1))
+                    logger.info('a11 unknown2 => ' + str(self.a11unknown2))
+                    logger.info('a11 unknown3 => ' + str(self.a11unknown3))
+                    logger.info('a11 unknown4 => ' + str(self.a11unknown4))
+                    logger.info('a11 unknown5 => ' + str(self.a11unknown5))
+                    logger.info('a11pvInputPower => ' + str(self.a11pvInputPower))
+                    logger.info('a11pvInputVoltage => ' + str(self.a11pvInputVoltage))
+                    logger.info('a11 unknown8 => ' + str(self.a11unknown8))
+                    logger.info('a11 unknown9 => ' + str(self.a11unknown9))
+                    logger.info('calculated values')
+                    logger.info('a11pvInputCurrent => ' + str(self.a11pvInputCurrent))
+                    logger.info('------------------------')
+
+            if self.a29Data:
+                # A29 Bereich
+                self.a29unknown0 = self.a29Data[0]  #
+                self.a29unknown1 = self.a29Data[1]  #
+                self.a29unknown2 = self.a29Data[2]  #
+                self.a29unknown3 = self.a29Data[3]  #
+                self.a29unknown4 = self.a29Data[4]  #
+                self.a29unknown5 = self.a29Data[5]  #
+                self.a29unknown6 = self.a29Data[6]  #
+                self.a29unknown7 = self.a29Data[7]  #
+                self.a29unknown8 = self.a29Data[8]  #
+                self.a29unknown9 = self.a29Data[9]  #
+
+                if self.debug:
+                    # AUSGABE
+                    logger.info('Collected values oa A29')
+                    logger.info('------------------------')
+                    logger.info('a29 unknown0 => ' + str(self.a29unknown0))
+                    logger.info('a29 unknown1 => ' + str(self.a29unknown1))
+                    logger.info('a29 unknown2 => ' + str(self.a29unknown2))
+                    logger.info('a29 unknown3 => ' + str(self.a29unknown3))
+                    logger.info('a29 unknown4 => ' + str(self.a29unknown4))
+                    logger.info('a29 unknown5 => ' + str(self.a29unknown5))
+                    logger.info('a29 unknown6 => ' + str(self.a29unknown6))
+                    logger.info('a29 unknown7 => ' + str(self.a29unknown7))
+                    logger.info('a29 unknown8 => ' + str(self.a29unknown8))
+                    logger.info('a29 unknown9 => ' + str(self.a29unknown9))
+                    logger.info('------------------------')
+
         self.newData = False
         return True
 
-    def generalDataCB(self, data, charType):
+    def generalDataCB(self, outbackServiceData):
         with self.mutex:
-            if self.debug:
-                logger.info('setting data for ' + "" + charType)
-            if charType == "a03":
-                self.a03Data = data
-                logger.info("a03 => " + str(data))
-            elif charType == "a11":
-                self.a11Data = data
-                logger.info("a11 => " + str(data))
-            elif charType == "a29":
-                self.a29Data = data
-                logger.info("a29 => " + str(data))
-            else:
+            for charType, data in outbackServiceData.items():
                 if self.debug:
-                    logger.warning("no characteristic given")
+                    logger.info('setting data for ' + "" + charType)
+                if charType == "a03":
+                    self.a03Data = data
+                    logger.info("a03 => " + str(data))
+                elif charType == "a11":
+                    self.a11Data = data
+                    logger.info("a11 => " + str(data))
+                elif charType == "a29":
+                    self.a29Data = data
+                    logger.info("a29 => " + str(data))
+                else:
+                    if self.debug:
+                        logger.warning("no characteristic given")
+
             logger.info("setting new data => True")
             self.newData = True
-
 
 # Testmethode für direkten Aufruf
 if __name__ == "__main__":
